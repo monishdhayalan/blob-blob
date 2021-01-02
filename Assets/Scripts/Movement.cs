a078a819de012a4b460f58ed27d2375ab07dc0f6
Vinayaga Pillai <take2vp@gmail.com>
2021-01-02 16:20:13 +0530

    unity files

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+
+    Rigidbody2D rigidbody2d;
+
+    [Header("Movement Settings")]
+    [SerializeField] float moveSpeed = 5f;
+    [SerializeField] float jumpPower = 10f;
+
+    [Header("Bools")]
+    [SerializeField] bool isGrounded = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Vector3 Move = new Vector3(Input.GetAxis("Horizontal"), 0, 0);
+        transform.position += Move * Time.deltaTime * moveSpeed;
+        Jump();
+
+    }
+
+    void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded == true)
+        {
+            rigidbody2d.AddForce(new Vector2(0f, jumpPower), ForceMode2D.Impulse);
+            isGrounded = false;
+            //StartCoroutine(QuickFall());
+        }
+    }
+
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        isGrounded = true;
+    }
+
+}
---
+++ Assets/Scripts/SoftBody.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.U2D;
+
+public class SoftBody : MonoBehaviour
+{
+
+    #region constants
+    private const float splineOffset = 0.5f;
+    #endregion
+
+    #region Feilds
+    [SerializeField] public SpriteShapeController spriteShape;
+    [SerializeField] public Transform[] points;
+    #endregion
+
+    #region Monnobehaviour
+    private void Awake()
+    {
+        UpdateVertices();
+    }
+
+    private void Update()
+    {
+        UpdateVertices();
+    }
+
+    #endregion
+
+    #region private functions
+
+    private void UpdateVertices()
+    {
+        for (int i = 0; i < points.Length - 1; i++)
+        {
+            Vector2 _vertex = points[i].localPosition;
+
+            Vector2 _towardCenter = (Vector2.zero - _vertex).normalized;
+
+            float _colliderRadius = points[i].gameObject.GetComponent<CircleCollider2D>().radius;
+
+            try
+            {
+                spriteShape.spline.SetPosition(i, (_vertex - _towardCenter * _colliderRadius));
+            }
+            catch
+            {
+                spriteShape.spline.SetPosition(i, (_vertex - _towardCenter * (_colliderRadius + splineOffset)));
+            }
+
+            Vector2 _lt = spriteShape.spline.GetLeftTangent(i);
+
+            Vector2 _newRt = Vector2.Perpendicular(_towardCenter) * _lt.magnitude;
+            Vector2 _newLt = Vector2.zero - _newRt;
+
+            spriteShape.spline.SetRightTangent(i, _newLt);
+            spriteShape.spline.SetLeftTangent(i, _newRt);
+        }
+
+    }
+
+    #endregion
+
+}
